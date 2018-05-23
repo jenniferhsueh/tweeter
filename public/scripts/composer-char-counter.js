a85cf416e6e63214f05cc9c4c5f58fc03831c5bf
jenniferhsueh <jenhsueh89@hotmail.com>
2018-05-23 18:57:14 +0000

    added character count feature

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,14 @@
+$(document).ready(function(event) {
+  const counter = $(".counter");
+  $("textarea").on("input", function(event) {
+    const tweet = $(this).val(); //event.target.value.length
+    const textCounter = 140 - tweet.length;
+    const textRemaining = counter.text(textCounter);
+    if (textCounter < 0) {      //text dom text node
+      $(textRemaining).css({color: 'red'});
+    } else {
+      $(textRemaining).css({color: 'black'});
+    }
+  }) 
+});
+
