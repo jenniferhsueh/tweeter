52a2f2dfb1b3b4a17d9b78076d2d16702ff3061c
jenniferhsueh <jenhsueh89@hotmail.com>
2018-05-24 00:43:57 +0000

    Implement createTweetElement function

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,4 +3,54 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+const tweetData = {
+  "user": {
+    "name": "Newton",
+    "avatars": {
+      "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+      "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+      "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+    },
+    "handle": "@SirIsaac"
+  },
+  "content": {
+    "text": "If I have seen further it is by standing on the shoulders of giants"
+  },
+  "created_at": 1461116232227
+}
 
+$(document).ready(() => {
+  function createTweetElement(tweet) {
+    let $article = $("<article>");
+    let $header = $("<header>");
+    let $avatar = $("<img class='bunny'>").attr("src", tweet.user.avatars.small); 
+    let $username = $("<h2>").text(tweet.user.name);
+    let $handle = $("<span>").addClass("handle").text(tweet.user.handle);
+    let $br = $("<br>");
+    let $content = $("<div>").addClass("tweet-content").text(tweet.content.text);
+    let $footer = $("<footer>");
+    let $createdAt = $("<div>").text(tweet.created_at);
+    // let $icons = $("<div>").addClass("icon").attr("src");
+    
+    $article.append($header);
+    $header.append($avatar);
+    $header.append($username);
+    $username.append($handle);
+    $article.append($br);
+    $article.append($content);
+    $article.append($br);
+    $article.append($footer);
+    $footer.append($createdAt);
+    // $footer.append($icons);
+
+    return $article;
+  }
+
+
+var $tweet = createTweetElement(tweetData);
+
+console.log($tweet); // to see what it looks like
+
+$('.tweet').append($tweet);
+// var $tweet = $("<article>").addClass("tweet");
+});
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,9 +1,8 @@
 $(document).ready(function(event) {
-  const counter = $(".counter");
-  $("textarea").on("change", function(event) {
+  $("textarea").on("input", function(event) {
     const tweet = $(this).val(); //event.target.value.length
     const textCounter = 140 - tweet.length;
-    const textRemaining = counter.text(textCounter);
+    const textRemaining = $(".counter").text(textCounter);
     if (textCounter < 0) {      //text dom text node
       $(textRemaining).css({color: 'red'});
     } else {
