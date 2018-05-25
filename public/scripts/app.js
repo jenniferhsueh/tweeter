24b81d122d466f3e0074f3e29fd6e28a6ed91b41
jenniferhsueh <jenhsueh89@hotmail.com>
2018-05-25 02:07:32 +0000

    code review

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,66 +3,22 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-const localdata = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": {
-        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-      },
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": {
-        "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-        "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-      },
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  },
-  {
-    "user": {
-      "name": "Johann von Goethe",
-      "avatars": {
-        "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-        "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-      },
-      "handle": "@johann49"
-    },
-    "content": {
-      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-    },
-    "created_at": 1461113796368
-  }
-];
 
-$(document).ready(() => {
+$(document).ready(function() {
   function renderTweets(tweets) {
     $("#tweet-list").empty();
     for (var i = 0; i < tweets.length; i++) {
       let tweet = tweets[i];
-      const $tweet = createTweetElement(tweet); //readability
-      $("#tweet-list").prepend($tweet); //append to tweet section in index.html
+      const $tweet = createTweetElement(tweet);
+      $("#tweet-list").prepend($tweet);
     }
   }
 
   function createTweetElement(tweet) {
+     // let $article = $("<article class='tweet'>")
     let $article = $("<article>").addClass("tweet");
     let $header = $("<header>");
+    // let $avatar = $(`<img src="${tweet.whatever.src}" class="avatar">`)
     let $avatar = $("<img>").addClass("avatar").attr("src", tweet.user.avatars.small); 
     let $username = $("<h2>").text(tweet.user.name);
     let $handle = $("<span>").addClass("handle").text(tweet.user.handle);
@@ -74,12 +30,22 @@ $(document).ready(() => {
     let $retweet = $("<img>").addClass("icon").attr("src", "/images/retweet.png");
     let $flag = $("<img>").addClass("icon").attr("src", "/images/flag.png");
 
-    $article.append($header).append($br).append($content).append($br).append($footer);
-    $header.append($avatar).append($username);
+    $article.append(($header), ($br), ($content), ($br), ($footer));
+    $header.append(($avatar), ($username));
     $username.append($handle);
-    $footer.append($createdAt).append($heart).append($retweet).append($flag);
+    $footer.append(($createdAt), ($heart), ($retweet), ($flag));
 
     return $article;
+    // Prep the whole thing at once
+    // var $article = $(`<div class="tweet">
+    //   <h2></h2>
+    // </div>`);
+
+    // use .find to look for specific template parts and set them
+    // $article.find('h2').setText(username);
+
+    // !!!OR!!! just use something like handlebars or lit-html
+  }
   }
 
   $("form").on("submit", function(event) {
@@ -102,7 +68,6 @@ $(document).ready(() => {
     $.ajax({
       url: "/tweets",
       method: "GET",
-      // data: tweet,
       success: function (data) {
         renderTweets(data);
       }
@@ -115,7 +80,7 @@ $(document).ready(() => {
     });
   }
 
-loadTweets();
+  loadTweets();
 
 
   $(".compose").click(function(){
